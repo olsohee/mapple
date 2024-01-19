00a7ea202a80e57b8c6a0642fd69eaceaac63eaf
olsohee <dlthgml0108@daum.net>
2024-01-19 11:53:09 +0900

    feat: setup mapple project

---
+++ src/main/java/mapple/mapple/MappleApplication.java
@@ -0,0 +1,13 @@
+package mapple.mapple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mapple/mapple/MappleApplicationTests.java
@@ -0,0 +1,13 @@
+package mapple.mapple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
