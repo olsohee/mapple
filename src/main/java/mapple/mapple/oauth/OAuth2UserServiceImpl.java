d6a0bb08f599cfa8920f0f15b78dce498f2f33cc
olsohee <dlthgml0108@daum.net>
2024-01-27 16:51:35 +0900

    refactor: OAuth2UserServiceImpl 식별자 추출 메소드 생성

--- src/main/java/mapple/mapple/oauth/OAuth2UserServiceImpl.java
+++ src/main/java/mapple/mapple/oauth/OAuth2UserServiceImpl.java
@@ -24,23 +24,13 @@ public class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         log.info("OAuth2UserServiceImpl 실행");
-        OAuth2User oAuth2User = super.loadUser(userRequest);
 
+        OAuth2User oAuth2User = super.loadUser(userRequest);
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuthProvider oAuthProvider = OAuthProvider.find(registrationId);
+        String identifier = getIdentifier(oAuthProvider, oAuth2User);
 
-        log.info("oAuth2User = {}", oAuth2User);
-        String identifier="";
-        if (oAuthProvider == OAuthProvider.KAKAO) {
-            identifier = oAuth2User.getName();
-
-        } else if (oAuthProvider == OAuthProvider.NAVER) {
-            String[] arr = oAuth2User.getName().split(", ");
-            identifier = arr[0].substring(4);
-        }
-        log.info("identifier = {}", identifier);
-
-        // DB에 없으면 회원가입
+        // 해당 identifier가 DB에 저장되어 있지 않은 식별자이면 회원가입
         Optional<User> optionalUser = userRepository.findByIdentifier(identifier);
         if (optionalUser.isEmpty()) {
             User user = User.createOAuthUser(identifier, oAuthProvider);
@@ -48,5 +38,17 @@ public class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
         }
 
         return super.loadUser(userRequest);
+    }
+
+    private String getIdentifier(OAuthProvider oAuthProvider, OAuth2User oAuth2User) {
+        if (oAuthProvider == OAuthProvider.KAKAO) {
+            return oAuth2User.getName();
+
+        }
+        else if (oAuthProvider == OAuthProvider.NAVER) {
+            String[] arr = oAuth2User.getName().split(", ");
+            return arr[0].substring(4);
+        }
+        return null;
     }
 }
